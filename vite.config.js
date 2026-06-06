03d3db8ead69ea7b73daf43052b688274fd31555
jephat <mukundijephat22@gmail.com>
2026-06-06 11:27:02 +0300

    deploying: downgraded tailwind to  v3

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,17 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [
-//     react(), 
-//     tailwindcss()
-//   ],
-//   server: {
-//     allowedHosts: true,
-//     host: "0.0.0.0",
-//     port: 3000,
-//   },
-// })
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // 1. Swapped to the correct plugin
-import tailwindcss from '@tailwindcss/vite' 
+import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(), // 2. Works perfectly with Tailwind v4 and React
-    tailwindcss() 
+    react(), 
+    // tailwindcss()
   ],
   server: {
-    allowedHosts: ['weatherassig.onrender.com'],
+    allowedHosts: true,
     host: "0.0.0.0",
     port: 3000,
-  }
+  },
 })
+
+
