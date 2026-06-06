4ab35b692738abdb9ddc8764893f52d5e41287f5
jephat <mukundijephat22@gmail.com>
2026-06-06 10:50:00 +0300

    deploying: changed some settings

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
 
+// export default defineConfig({
+//   plugins: [
+//     react(), 
+//     tailwindcss()
+//   ],
+//   server: {
+//     allowedHosts: true,
+//     host: "0.0.0.0",
+//     port: 3000,
+//   },
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()
-  ],
+  plugins: [react()],
   server: {
-    allowedHosts: true,
+    host: "0.0.0.0",
     port: 3000,
   },
-})
+});
